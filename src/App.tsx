b3dfb0c7d01f557bc928359efa85226e0ebc94ea
rodeox1980 <rodeox.1980@gmail.com>
2022-09-02 23:10:10 +0900

    [NEW]新規作成

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>Hello, todolist</div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@/",
+        replacement: `${path.resolve(__dirname, 'src')}/`
+      },
+    ],
+  },
+});
